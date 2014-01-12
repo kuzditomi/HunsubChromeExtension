eb32111d9939a5ac36342ef78a475346cb43f07e
Küzdi Tamás <kuzditomi@gmail.com>
2014-01-12 15:43:18 +0100

    v1.0 working version

---
+++ src/blocker.js
@@ -0,0 +1,88 @@
+var a = document.querySelector('.tbb');
+var userDivs;
+var blockQuotes = false;
+if(a){
+	userDivs = document.querySelectorAll('form[name=REPLIER] > dl');
+	chrome.storage.sync.get('blockQuotes', function(data){
+		blockQuotes = data && data.blockQuotes;
+		blockUsers(blockQuotes);
+	});
+
+	addButtons();
+}
+
+function addButtons(){
+	for(var i =0;i < userDivs.length-1;i++){
+		var userDiv = userDivs[i];
+		var links = userDiv.querySelector('.fR.links');
+		var blockLink = document.createElement('a');
+		
+		if(!links || !blockLink)
+			continue;
+			
+		var nameAnchor = userDiv.querySelector('dt a');
+		var start = nameAnchor.href.indexOf('user.php?i=');
+		var userId = nameAnchor.href.substring(start+11,nameAnchor.length);
+		var userName = nameAnchor.textContent;
+		(function(userId,userName){
+			blockLink.addEventListener('click',function() {
+				blockUser(userId,userName);
+			});
+		})(userId,userName);
+
+		blockLink.textContent = "block";
+		blockLink.style.color = "red";
+		blockLink.style.cursor = "pointer";
+		links.appendChild(blockLink);
+	}
+}
+
+function blockUsers(blockQuotes){
+	console.log('starting');
+	chrome.storage.sync.get('blockedUsers', function(data){
+		if(!data || !data.blockedUsers)
+			var blockedUsers = {};
+		else
+			var blockedUsers = data.blockedUsers;
+		
+		userDivs = document.querySelectorAll('form[name=REPLIER] > dl');
+		for(var i =0;i < userDivs.length-1;i++){
+			var userDiv = userDivs[i];
+			var nameAnchor = userDiv.querySelector('dt a');
+			var start = nameAnchor.href.indexOf('user.php?i=');
+			var userId = nameAnchor.href.substring(start+11,nameAnchor.length);
+			if(blockedUsers[userId]){
+				userDiv.parentNode.removeChild(userDiv);
+			}else if(blockQuotes){
+				var quoteDivs = userDiv.querySelectorAll('.quote1');
+				for(var j = 0;j<quoteDivs.length;j++){
+					var quoteDiv = quoteDivs[j];
+					var userName = quoteDiv.textContent.match(/Quote\s\(([^\s]+)/)[1];
+					for(var prop in blockedUsers) {
+						if(blockedUsers.hasOwnProperty(prop)) {
+							if(blockedUsers[prop] === userName) {
+								var quote = quoteDiv.nextSibling;
+								quoteDiv.parentNode.removeChild(quoteDiv);
+								quote.parentNode.removeChild(quote);
+							}
+						}
+					}
+				}
+			}
+		}
+	});	
+}
+
+function blockUser(userId,userName){
+	chrome.storage.sync.get('blockedUsers', function(data){
+		if(!data || !data.blockedUsers)
+			var blockedUsers = {};
+		else
+			var blockedUsers = data.blockedUsers;
+		
+		blockedUsers[userId] = userName;
+		chrome.storage.sync.set({'blockedUsers': blockedUsers},function(){
+			blockUsers(blockQuotes);
+		});
+	});
+}
---
+++ src/popup.js
@@ -0,0 +1,56 @@
+var blockedUsers;
+document.addEventListener('DOMContentLoaded', function () {
+	chrome.storage.sync.get('blockQuotes', function(data){
+		if(!data || !data.blockQuotes)
+			var blockQuotes = false;
+		else
+			var blockQuotes = data.blockQuotes;
+			
+		var cbBlockQuotes = document.getElementById('blockQuotes');
+		cbBlockQuotes.checked = blockQuotes;
+		cbBlockQuotes.addEventListener('click',function(){
+			blockQuotes = cbBlockQuotes.checked;
+			chrome.storage.sync.set({'blockQuotes':blockQuotes});
+		});
+	});
+	//alert(cbBlockQuotes.value);
+  	chrome.storage.sync.get('blockedUsers', function(data){
+		blockedUsers = data.blockedUsers;
+		var usersdiv = document.getElementById('blockedUsers');
+		for(var i in blockedUsers){
+			var userDiv = document.createElement('div');
+			userDiv.setAttribute('class','user');
+			userDiv.setAttribute('id',blockedUsers[i]);
+			
+			var userNameSpan = document.createElement('span');
+			userNameSpan.textContent = blockedUsers[i];
+			
+			var enableButton = document.createElement('input');
+			enableButton.setAttribute('type','button');
+			enableButton.setAttribute('class','enable');
+			(function(i){
+				enableButton.addEventListener('click',function() {
+					var deleteDiv = document.getElementById(blockedUsers[i]);
+					delete blockedUsers[i];
+					chrome.storage.sync.set({'blockedUsers': blockedUsers},function(){
+						deleteDiv.parentNode.removeChild(deleteDiv);
+					});
+				});
+			})(i);
+			enableButton.setAttribute('value',"unblock");
+
+			userDiv.appendChild(userNameSpan);
+			userDiv.appendChild(enableButton);
+			usersdiv.appendChild(userDiv);
+		}
+	});	
+});
+
+var enableHandler = function(i){
+	return (function(){
+		delete blockedUsers[i];
+		chrome.storage.sync.set({'blockedUsers': blockedUsers},function(){
+			alert('ok');
+		});
+	})(i);
+}
